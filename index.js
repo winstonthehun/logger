e54dfc686b2a256f680cdc92740762b3dca466a1
Winston Sebesta <winston.sebesta@maze.design>
2022-08-13 09:23:06 +0200

    refactor: simplify a bit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const currentLogLevel = process.env.LOG_LEVEL || "info";
-const LogLevels = { error: 4, warn: 3, info: 2, log: 1, debug: 0 };
-
-const sensitiveKeys = [
-  "secret",
-  "jwt",
-  "token",
-  "refresh_token",
-  "password",
-  "authorization",
-];
-
 const redact = (k, v) =>
-  sensitiveKeys.includes(k.toLowerCase()) ? "REDACTED" : v;
+  [
+    "secret",
+    "jwt",
+    "token",
+    "refresh_token",
+    "password",
+    "authorization",
+  ].includes(k.toLowerCase())
+    ? "REDACTED"
+    : v;
+
+const envLogLevel = process.env.LOG_LEVEL || "info";
+const LogLevels = { error: 4, warn: 3, info: 2, log: 1, debug: 0 };
 
 const log = (level) => (message, data) => {
   const isProduction = process.env.NODE_ENV === "production";
   const isBrowser = typeof window !== "undefined";
 
   // swallow logs with log levels below current config
-  if (LogLevels[level] < LogLevels[currentLogLevel]) {
+  if (LogLevels[level] < LogLevels[envLogLevel]) {
     return () => null;
   }
 
